72cc013d8aaccc8606b8fbee7dc34698e67f750d
Cesar Calderon <cesar.calderon@airtable.com>
2024-09-11 08:52:55 -0500

    Testing pkg size action with light library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import dayjs from "dayjs";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1>{dayjs(new Date).format('[Today is] dddd')}</h1>
       <main className={styles.main}>
         <Image
           className={styles.logo}
